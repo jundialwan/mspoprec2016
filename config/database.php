346a3fbe8ba0c1774f3cd100e558ebf8afa9095a
jundialwan <jundi.alwan@gmail.com>
2016-05-03 02:07:45 +0700

    add database configuration

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'jundialwan.database.windows.net'),
+            'port' => env('DB_PORT', '1433'),
+            'database' => env('DB_DATABASE', 'mspoprec2016'),
+            'username' => env('DB_USERNAME', 'jundialwan'),
+            'password' => env('DB_PASSWORD', 'MSPOprec2016'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
