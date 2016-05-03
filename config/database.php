893321bef412e7790bf7eed0ed491d3d32e3700f
jundialwan <jundi.alwan@gmail.com>
2016-05-03 20:26:30 +0700

    update database settings

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'jundialwan.database.windows.net'),
-            'port' => env('DB_PORT', '1433'),
+            'host' => env('DB_HOST', 'br-cdbr-azure-south-b.cloudapp.net'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'mspoprec2016'),
-            'username' => env('DB_USERNAME', 'jundialwan'),
-            'password' => env('DB_PASSWORD', 'MSPOprec2016'),
+            'username' => env('DB_USERNAME', 'b2147ca6ebb12f'),
+            'password' => env('DB_PASSWORD', '291ac707'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
