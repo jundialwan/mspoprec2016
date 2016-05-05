2c2f7e536ffe87dc0ac1de37fcd1300b7f2ecd7b
jundialwan <jundi.alwan@gmail.com>
2016-05-05 08:55:22 +0700

    add some css styling for color

---
+++ resources/views/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE Html>
+<html>
+    <head>
+        <title>jundialwan - {{ $data['title'] }}</title>
+        <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ url('css/main.css') }}">
+        
+        <script src="{{ url('js/jquery.min.js') }}"></script>
+        <script src="{{ url('js/main.js') }}"></script>
+    </head>
+    <body>
+         
+    </body>
+</html>
